0ce0e1c4d9e156ee49f13b809b02413a52017e4a
Vinizix11 <marcos.monteiro.moreira@escola.pr.gov.br>
2026-04-24 10:48:27 -0300

    kkkk

---
+++ script.js
@@ -0,0 +1,9 @@
+function salvarCarro(event) {
+    event.preventDefault();
+
+    let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
+    
+}
