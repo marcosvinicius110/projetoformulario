8e08c8768d2d941a9f987555174dfe6122700053
Vinizix11 <marcos.monteiro.moreira@escola.pr.gov.br>
2026-04-24 11:15:03 -0300

    kkkkkkkk

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ function salvarCarro(event) {
     let preco = document.getElementById('preco').value;
     let marca = document.getElementById('marca').value;
     let modelo = document.getElementById('modelo').value;
-    
+
+    let cambioSelecionado = document.querySelector('input [name="marcha"]:checked');
+
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "não informado";
+
+    let carro = {
+        titulo,
+        preco,
+        marca,
+        modelo,
+        cambio
+    };
+
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON,stringify(carros));
+
+    adcionarNaTela(carro);
+
+    document.querySelector("form").reset();
 }
